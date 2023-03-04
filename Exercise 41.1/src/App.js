b7b1e7ab65f76d515fc503995a449675e93bc596
Nidhi Satija <126676344+nidhi-codeyoung@users.noreply.github.com>
2023-03-04 21:59:22 +0530

    Add files via upload

---
+++ Exercise 41.1/src/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import FormContainer from "./Form/FormContainer";
+import './App.css'
+
+function App() {
+  return <FormContainer />;
+}
+export default App;
